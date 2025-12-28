5abdd5fc39f565ec39a60d77c9834e9f57e07802
relir55 <pashaananin70@gmail.com>
2025-12-28 19:21:35 +0500

    добавилли команду креатор привет

--- bot.py
+++ bot.py
@@ -24,3 +24,9 @@ def ban_user(message):
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
 
 bot.infinity_polling(none_stop=True)
+
+
+
+
+
+print('hello creator')
